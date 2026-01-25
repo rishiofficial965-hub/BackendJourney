d9af175fff0f60a4860846a9eaf76bc7a9b2622d
Rishi <rishiofficial965@gmail.com>
2026-01-26 01:31:32 +0530

    starting db

---
+++ day2DATABASE/server.js
@@ -0,0 +1,3 @@
+const app = require("./src/app1")
+
+app.listen(3000,()=>console.log("server is running......"))
---
+++ day2DATABASE/src/app1.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.status(201).json({
+    message: "note created successfully",
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    notes:notes
+  })
+});
+
+app.delete("/notes/:id",(req,res)=>{
+    let idx = req.params.id
+    delete notes[idx]
+    res.status(204).json()
+})
+module.exports = app;
