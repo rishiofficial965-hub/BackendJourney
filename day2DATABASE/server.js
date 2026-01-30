5b4d976d1b7abd899d90d549abc8a48dd6a84e93
Rishi <rishiofficial965@gmail.com>
2026-01-31 01:48:12 +0530

    connected database and downloaded mongosse package

--- day2DATABASE/server.js
+++ day2DATABASE/server.js
@@ -1,4 +1,13 @@
-const app = require("./src/app1")
+const app = require("./src/app1");
+const mongoose = require("mongoose");
 
+function connectToDb() {
+  mongoose
+    .connect(
+      "mongodb+srv://rishi:VVOuAoNcKDNt3Mcz@cluster0.qi2thxz.mongodb.net/day-2",
+    )
+    .then(() => console.log("connected to database"));
+}
+connectToDb();
 //starting server on port....
-app.listen(3000,()=>console.log("server is running......"))
+app.listen(3000, () => console.log("server is running......"));
