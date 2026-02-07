933bd316d5eae29c2a848facbe27b2a5b4f0e9c7
Rishi <rishiofficial965@gmail.com>
2026-02-07 14:59:05 +0530

    auth & jwt & hashing

--- day4/src/routes/auth.routes.js
+++ day4/src/routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userModel = require("../models/user.model");
 const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto")
 
 authRouter.post("/register", async (req, res) => {
   const { email, name, password } = req.body;
@@ -14,10 +15,11 @@ authRouter.post("/register", async (req, res) => {
       message: "user already exist...",
     });
   }
+  const hash = crypto.createHash("md5").update(password).digest("hex")
   const user = await userModel.create({
     name,
     email,
-    password,
+    password:hash,
   });
   const token = jwt.sign(
     {
@@ -31,6 +33,34 @@ authRouter.post("/register", async (req, res) => {
     message: "user created",
     userDetails: user,
     token,
+  });
+});
+authRouter.post("/protected", async (req, res) => {});
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.send(404).json({
+      message: "user not exist....",
+    });
+  }
+  const hash = crypto.createHash("md5").update(password).digest("hex")
+  const pass = user.password == hash;
+  if (!pass) {
+    return res.status(401).json({
+      message: "wrong password....",
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
+  res.cookie("jwt_token", token);
+  res.status(200).json({
+    message: "user logged in successfully....",
   });
 });
 
